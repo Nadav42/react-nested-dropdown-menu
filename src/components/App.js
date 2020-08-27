801f847b9f34f875d3d0d6a2b93d253160b9cc6a
Nadav42 <bscpkd@Gmail.com>
2020-08-28 02:01:20 +0300

    fixed dropdown overflow out of screen

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { DropdownMenu, DropdownSubMenu, DropdownItem } from './Dropdown';
 import { MdDelete, MdTranslate } from "react-icons/md";
+import '../main.css';
 
 const ButtonTest = ({ toggleOpen }) => {
 	return (
@@ -10,20 +11,24 @@ const ButtonTest = ({ toggleOpen }) => {
 
 const ButtonTest2 = ({ toggleOpen }) => {
 	return (
-		<button className="btn btn-primary" onClick={toggleOpen}>X</button>
+		<button className="btn btn-primary" onClick={(e) => toggleOpen(e)}>X</button>
 	);
 }
 
 const App = () => {
 	return (
-		<div className="container mt-5">
+		<div className="container mt-5 drop-test">
 			<span>
 				<DropdownMenu Button={ButtonTest}>
 					<DropdownSubMenu>
-						<DropdownItem title="Device" onClick={() => { console.log("clicked item #1") }} switchToMenu="devices" />
+						<DropdownItem title="Device" switchToMenu="devices" />
 						<DropdownItem title="Theme" switchToMenu="themes" />
 						<DropdownItem title="Set alt text" />
 						<DropdownItem title="Remove" rightIcon={<MdDelete />} rightCircle />
+					</DropdownSubMenu>
+					<DropdownSubMenu menuKey="devices" >
+						<DropdownItem title="Desktop" />
+						<DropdownItem title="Mobile" />
 					</DropdownSubMenu>
 					<DropdownSubMenu menuKey="themes" >
 						<DropdownItem title="Game" />
@@ -43,6 +48,32 @@ const App = () => {
 				<DropdownMenu Button={ButtonTest2}>
 					<DropdownItem title="Translations" rightIcon={<MdTranslate />} />
 					<DropdownItem title="Remove" rightIcon={<MdDelete />} />
+				</DropdownMenu>
+			</span>
+			<span>
+				<DropdownMenu Button={ButtonTest2}>
+					<DropdownSubMenu>
+						<DropdownItem title="Device" switchToMenu="devices" />
+						<DropdownItem title="Theme" switchToMenu="themes" />
+						<DropdownItem title="Set alt text" />
+						<DropdownItem title="Remove" rightIcon={<MdDelete />} rightCircle />
+					</DropdownSubMenu>
+					<DropdownSubMenu menuKey="devices" >
+						<DropdownItem title="Desktop" />
+						<DropdownItem title="Mobile" />
+					</DropdownSubMenu>
+					<DropdownSubMenu menuKey="themes" >
+						<DropdownItem title="Game" />
+						<DropdownItem title="Paylines" />
+						<DropdownItem title="Big Win" />
+						<DropdownItem title="Free Spins" />
+						<DropdownItem title="Paytable" />
+						<DropdownItem title="Bonus" />
+					</DropdownSubMenu>
+					<DropdownSubMenu menuKey="language" >
+						<DropdownItem title="English" />
+						<DropdownItem title="French" />
+					</DropdownSubMenu>
 				</DropdownMenu>
 			</span>
 		</div >
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -2,6 +2,11 @@ import React, { useState, useRef, useEffect } from 'react';
 import { MdArrowForward, MdArrowBack } from "react-icons/md";
 import "./Dropdown.css";
 
+// stop react warnings on empty href
+const LinkWithoutWarnings = ({ className, onClick, href, children }) => {
+    return <a className={className} onClick={onClick} href={href}>{children}</a>;
+}
+
 // hook
 function useOutsideAlerter(ref, callback) {
     useEffect(() => {
@@ -20,16 +25,101 @@ function useOutsideAlerter(ref, callback) {
     }, [ref, callback]);
 }
 
+// hook
+function useWindowSize() {
+    const [windowSize, setWindowSize] = useState({ width: undefined, height: undefined });
+
+    useEffect(() => {
+        function handleResize() {
+            setWindowSize({ width: window.innerWidth, height: window.innerHeight });
+        }
+
+        // Add event listener
+        window.addEventListener("resize", handleResize);
+
+        // Call handler right away so state gets updated with initial window size
+        handleResize();
+
+        // Remove event listener on cleanup
+        return () => window.removeEventListener("resize", handleResize);
+    }, []); // Empty array ensures that effect is only run on mount
+
+    return windowSize;
+}
+
 // for inner usage
-const DropdownMenuBody = ({ open, children }) => {
+const DropdownMenuBody = ({ open, xClickPos, yClickPos, activeMenu, children }) => {
+    const menuRef = useRef(null);
+    const [xPos, setXPos] = useState(xClickPos);
+    const [yPos, setYPos] = useState(yClickPos);
+    const screenSize = useWindowSize();
+
+    const spaceFromMouse = 5; // a bit of spacing from the click pos
     const baseClass = open ? "dropdown-menu show" : "dropdown-menu";
+    let dropdownStyle = {};
+
+    if (xPos != null && yPos != null) {
+        dropdownStyle = { position: "fixed", top: yPos, left: xPos + spaceFromMouse };
+    }
+
+    useEffect(() => {
+        if (!open || xClickPos == null || yClickPos == null || !menuRef.current) {
+            return;
+        }
+
+        const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+        const screenHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+        let xLeft = xClickPos;
+        let yTop = yClickPos;
+        let tooltipHeight = menuRef.current.scrollHeight;
+        let tooltipWidth = menuRef.current.scrollWidth;
+
+        let needsFixing = false;
+
+        // out of right border
+        if (xLeft > screenWidth - tooltipWidth) {
+            xLeft = xClickPos - tooltipWidth - (2 * spaceFromMouse);
+            needsFixing = true;
+        }
+
+        // out of bottom border
+        if (yTop > screenHeight - tooltipHeight) {
+            //yTop = yTop - tooltipHeight - (2 * spaceFromMouse);
+            yTop = yTop - tooltipHeight; //+ itemRect.height;
+            needsFixing = true;
+        }
+
+        // if it goes out of right edge of screen AND left edge of screen then try putting it in the middle
+        if (needsFixing && xLeft < 0) {
+            let freeSpace = Math.max(screenWidth - tooltipWidth, 0); // if there is no space (negative) take 0
+            xLeft = 0 + (freeSpace / 2) - (2 * spaceFromMouse);
+        }
+
+        // if it goes out of top edge of screen AND bottom edge of screen then try putting it in the middle
+        if (needsFixing && yTop < 0) {
+            let freeSpace = Math.max(screenHeight - tooltipHeight, 0); // if there is no space (negative) take 0
+            //yTop = 0 + (freeSpace / 2) - (2 * spaceFromMouse);
+            yTop = 0 + (freeSpace / 2);
+        }
+
+        if (xPos !== xLeft || yPos !== yTop) {
+            needsFixing = true;
+        }
+
+        if (needsFixing) {
+            console.log("needsFixing")
+            setXPos(xLeft);
+            setYPos(yTop);
+        }
+    }, [open, xClickPos, yClickPos, activeMenu, screenSize, menuRef])
 
     if (!open) {
         return null;
     }
 
     return (
-        <div className={baseClass}>
+        <div className={baseClass} style={dropdownStyle} ref={menuRef}>
             {children}
         </div>
     );
@@ -61,44 +151,44 @@ export const DropdownItem = ({ title, switchToMenu, changeActiveMenu, closeMenu,
 
     if (leftIcon && rightIcon) {
         return (
-            <a className={containerClass} href="#" onClick={handleClick}>
+            <LinkWithoutWarnings className={containerClass} onClick={handleClick}>
                 <div className={`dropdown-item-title`}>
                     <span className={leftIconClass}>{leftIcon}</span>
                     <span className="title-text">{title}</span>
                 </div>
                 <span className={rightIconClass}>{rightIcon}</span>
-            </a>
+            </LinkWithoutWarnings>
         );
     }
 
     if (rightIcon) {
         return (
-            <a className={containerClass} href="#" onClick={handleClick}>
+            <LinkWithoutWarnings className={containerClass} onClick={handleClick}>
                 <div className="dropdown-item-title">
                     <span className="title-text">{title}</span>
                 </div>
                 <span className={rightIconClass}>{rightIcon}</span>
-            </a>
+            </LinkWithoutWarnings>
         );
     }
 
     if (leftIcon) {
         return (
-            <a className={containerClass} href="#" onClick={handleClick}>
+            <LinkWithoutWarnings className={containerClass} onClick={handleClick}>
                 <div className="dropdown-item-title">
                     <span className={leftIconClass}>{leftIcon}</span>
                     <span className="title-text">{title}</span>
                 </div>
-            </a>
+            </LinkWithoutWarnings>
         );
     }
 
     return (
-        <a className={containerClass} href="#" onClick={handleClick}>
+        <LinkWithoutWarnings className={containerClass} onClick={handleClick}>
             <div className="dropdown-item-title">
                 <span className="title-text">{title}</span>
             </div>
-        </a>
+        </LinkWithoutWarnings>
     );
 }
 
@@ -122,7 +212,7 @@ export const DropdownSubMenu = ({ menuKey, activeMenu, changeActiveMenu, closeMe
             return (
                 <>
                     <DropdownItem title="Back" className="back-btn" onClick={() => changeActiveMenu(null)} leftIcon={<MdArrowBack />} />
-                    <div class="dropdown-divider"></div>
+                    <div className="dropdown-divider"></div>
                     {childrenWithProps}
                 </>
             );
@@ -135,12 +225,19 @@ export const DropdownSubMenu = ({ menuKey, activeMenu, changeActiveMenu, closeMe
 }
 
 // DropdownMenu
-export const DropdownMenu = ({ Button, children }) => {
+export const DropdownMenu = ({ Button, disableFixed, children }) => {
     const [isOpen, setIsOpen] = useState(false);
     const [activeMenu, setActiveMenu] = useState(null);
+    const [xClickPos, setXClickPos] = useState(null);
+    const [yClickPos, setYClickPos] = useState(null);
     const wrapperRef = useRef(null);
 
-    const toggleOpen = () => {
+    const toggleOpen = (e) => {
+        if (e && e.clientX && e.clientY && !disableFixed) {
+            setXClickPos(e.clientX);
+            setYClickPos(e.clientY);
+        }
+
         setIsOpen(!isOpen);
         setActiveMenu(null);
     }
@@ -148,6 +245,8 @@ export const DropdownMenu = ({ Button, children }) => {
     const closeMenu = () => {
         setIsOpen(false);
         setActiveMenu(null);
+        setXClickPos(null);
+        setYClickPos(null);
     }
 
     const changeActiveMenu = (menuKey) => {
@@ -168,9 +267,16 @@ export const DropdownMenu = ({ Button, children }) => {
         <>
             <div className="dropdown" style={{ display: "inline-block" }} ref={wrapperRef}>
                 <Button toggleOpen={toggleOpen} />
-                <DropdownMenuBody open={isOpen}>
-                    {childrenWithProps}
-                </DropdownMenuBody>
+                {isOpen ?
+                    (
+                        <DropdownMenuBody open={isOpen} xClickPos={xClickPos} yClickPos={yClickPos} activeMenu={activeMenu}>
+                            {childrenWithProps}
+                        </DropdownMenuBody>
+                    )
+                    : (
+                        null
+                    )
+                }
             </div>
         </>
     );
